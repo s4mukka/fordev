31110634be3e9d6e0b240454d05575e4900e80fa
Samuel Pereira <meninosam197@gmail.com>
2020-11-19 15:33:44 -0300

    test: fix url test

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -25,6 +25,15 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
+        const url = faker.internet.url()
+        const { sut, httpPostClientSpy } = makeSut(url)
+
+        await sut.auth(mockAuthentication())
+
+        expect(httpPostClientSpy.url).toBe(url)
+    })
+
+    test('Should call HttpPostClient with correct body', async () => {
         const { sut, httpPostClientSpy } = makeSut()
         const authenticationParams = mockAuthentication()
 
