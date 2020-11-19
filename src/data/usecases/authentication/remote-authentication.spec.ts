c089e45954c81381c42d7e58503242a87b1950e0
Samuel Pereira <meninosam197@gmail.com>
2020-11-19 16:05:01 -0300

    fix: adjust HttpStatusCode typo

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -48,7 +48,7 @@ describe('RemoteAuthentication', () => {
         const { sut, httpPostClientSpy } = makeSut()
 
         httpPostClientSpy.response = {
-            statusCode: HttpStatusCode.unathorized
+            statusCode: HttpStatusCode.unauthorized
         }
 
         const promise = sut.auth(mockAuthentication())
