fc6929ddf8801f5a723c73a806775d761c63b02e
Samuel Pereira <meninosam197@gmail.com>
2020-11-20 03:18:59 -0300

    feat: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -15,6 +15,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     const [state, setState] = useState({
         isLoading: false,
         email: '',
+        password: '',
         emailError: 'Campo obrigatório',
         passwordError: 'Campo obrigatório',
         messageError: ''
@@ -23,6 +24,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     useEffect(() => {
         validation.validate({ email: state.email })
     }, [state.email])
+
+    useEffect(() => {
+        validation.validate({ password: state.password })
+    }, [state.password])
 
     return (
         <div className={Styles.login}>
