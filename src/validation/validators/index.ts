6b4b8c9c27ba37d9aa909dced8ed26dea7ebdc55
Samuel Pereira <meninosam197@gmail.com>
2020-11-21 02:04:59 -0300

    feat: ensure ValidationBuilder has an email helper

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,4 +1,3 @@
-export * from './builder/validation-builder'
 export * from './email/email-validation'
 export * from './min-length/min-length-validation'
 export * from './required-field/required-field-validation'
