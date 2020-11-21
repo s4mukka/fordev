f991ca79f0680452cff6975caeb899ba4cab2553
Samuel Pereira <meninosam197@gmail.com>
2020-11-21 03:11:31 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -5,7 +5,11 @@ import '@/presentation/styles/global.scss'
 
 import Router from '@/presentation/router/router'
 
+import { makeLogin } from './factories/pages/login/login-factory'
+
 ReactDOM.render(
-    <Router />,
+    <Router
+        makeLogin={makeLogin}
+    />,
     document.getElementById('main')
 )
