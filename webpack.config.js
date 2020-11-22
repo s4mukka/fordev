f6f6ea1acb9d92973432556194937bfb671a9a70
Samuel Pereira <meninosam197@gmail.com>
2020-11-22 19:13:59 -0300

    fix: satart with webpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,8 @@ module.exports = {
     devServer: {
         contentBase: './public',
         writeToDisk: true,
-        historyApiFallback: true
+        historyApiFallback: true,
+        open: true
     },
     externals: {
         react: 'React',
