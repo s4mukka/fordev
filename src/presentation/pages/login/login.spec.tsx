c125966755f241978e28dc6c1d5a0bb53fb8f793
Samuel Pereira <meninosam197@gmail.com>
2020-11-25 19:01:21 -0300

    refactor: change link name

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -187,7 +187,7 @@ describe('Login Component', () => {
 
     test('Should go to signup page', () => {
         const { sut } = makeSut()
-        const register = sut.getByTestId('signup')
+        const register = sut.getByTestId('signup-link')
 
         fireEvent.click(register)
 
