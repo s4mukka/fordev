01fe95c08d8b0ab708648984651e40418196868a
Samuel Pereira <meninosam197@gmail.com>
2020-11-25 18:56:18 -0300

    test: ensure SignUp present erro if SaveAccessToken fails

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -163,7 +163,7 @@ describe('Login Component', () => {
         Helper.testChildCount(sut, 'error-wrap', 1)
     })
 
-    test('Should call SaveAccessToken on succes', async () => {
+    test('Should call SaveAccessToken on success', async () => {
         const { sut, authenticationSpy, saveAccessTokenMock } = makeSut()
 
         await simulateValidSubmit(sut)
@@ -177,7 +177,7 @@ describe('Login Component', () => {
         const { sut, saveAccessTokenMock } = makeSut()
         const error = new InvalidCredentialsError()
 
-        jest.spyOn(saveAccessTokenMock, 'save').mockReturnValueOnce(Promise.reject(error))
+        jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error)
 
         await simulateValidSubmit(sut)
 
