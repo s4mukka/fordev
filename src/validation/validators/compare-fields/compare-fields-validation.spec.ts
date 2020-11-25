5a850ebe8bf5b9f84c148d338af6ea53633e0612
Samuel Pereira <meninosam197@gmail.com>
2020-11-25 01:22:30 -0300

    test: ensure CompareFieldsValidation return falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -12,4 +12,12 @@ describe('CompareFieldsValidation', () => {
 
         expect(error).toEqual(new InvalidFieldError())
     })
+
+    test('Should return falsy if compare is valid', () => {
+        const valueToCompare = faker.random.word()
+        const sut = makeSut(valueToCompare)
+        const error = sut.validate(valueToCompare)
+
+        expect(error).toBeFalsy()
+    })
 })
