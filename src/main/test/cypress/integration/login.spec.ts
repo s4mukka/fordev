71b2cd2bfb1c3bd3406b3c05742667d36046dec0
Samuel Pereira <meninosam197@gmail.com>
2020-11-29 10:47:35 -0300

    refactor: change test to simulate submit with enter key

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -114,8 +114,7 @@ describe('Login', () => {
         })
 
         cy.getByTestId('email').focus().type(faker.internet.email())
-        cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
-        cy.getByTestId('submit').click()
+        cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5)).type('{enter}')
         cy.getByTestId('error-wrap')
         cy.getByTestId('spinner').should('not.exist')
         cy.getByTestId('message-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
