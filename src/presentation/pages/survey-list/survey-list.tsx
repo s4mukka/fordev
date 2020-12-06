558a668c807a28009fd753bf225139ae738ea54c
Samuel Pereira <meninosam197@gmail.com>
2020-12-06 19:10:11 -0300

    feat: add initial survey-list view

---
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import Styles from './survey-list-styles.scss'
+
+import { Footer, Logo } from '@/presentation/components'
+
+const SurveyList: React.FC = () => {
+    return (
+        <div className={Styles.surveyListWrap}>
+            <header className={Styles.headerWrap}>
+                <div className={Styles.headerContent}>
+                    <Logo />
+                    <div className={Styles.logoutWrap}>
+                        <span>Rodrigo</span>
+                        <a href="#">Sair</a>
+                    </div>
+                </div>
+            </header>
+            <div className={Styles.contentWrap}>
+                <h2>Enquetes</h2>
+                <ul>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <time>
+                                <span className={Styles.day}>28</span>
+                                <span className={Styles.month}>03</span>
+                                <span className={Styles.year}>2020</span>
+                            </time>
+                            <p>Qual é seu framework web favorito?</p>
+                        </div>
+                        <footer>Ver Resultado</footer>
+                    </li>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <time>
+                                <span className={Styles.day}>28</span>
+                                <span className={Styles.month}>03</span>
+                                <span className={Styles.year}>2020</span>
+                            </time>
+                            <p>Qual é seu framework web favorito?</p>
+                        </div>
+                        <footer>Ver Resultado</footer>
+                    </li>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <time>
+                                <span className={Styles.day}>28</span>
+                                <span className={Styles.month}>03</span>
+                                <span className={Styles.year}>2020</span>
+                            </time>
+                            <p>Qual é seu framework web favorito?</p>
+                        </div>
+                        <footer>Ver Resultado</footer>
+                    </li>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <time>
+                                <span className={Styles.day}>28</span>
+                                <span className={Styles.month}>03</span>
+                                <span className={Styles.year}>2020</span>
+                            </time>
+                            <p>Qual é seu framework web favorito?</p>
+                        </div>
+                        <footer>Ver Resultado</footer>
+                    </li>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <time>
+                                <span className={Styles.day}>28</span>
+                                <span className={Styles.month}>03</span>
+                                <span className={Styles.year}>2020</span>
+                            </time>
+                            <p>Qual é seu framework web favorito?</p>
+                        </div>
+                        <footer>Ver Resultado</footer>
+                    </li>
+                </ul>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default SurveyList
