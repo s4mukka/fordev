430a03eacd7a035805bf992d3bab623a052de62c
Samuel Pereira <meninosam197@gmail.com>
2020-12-10 20:48:46 -0300

    test: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { setCurrentAccountAdapter } from './current-account-adapter'
+import { setCurrentAccountAdapter, getCurrentAccountAdapter } from './current-account-adapter'
 
 import { mockAccountModel } from '@/domain/test'
 import { UnexpectedError } from '@/domain/errors'
@@ -7,7 +7,7 @@ import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 jest.mock('@/infra/cache/local-storage-adapter')
 
 describe('CurrentAccountAdapter', () => {
-    test('Should call LocalStorageAdapter with correct values', () => {
+    test('Should call LocalStorageAdapter.set with correct values', () => {
         const account = mockAccountModel()
         const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
         setCurrentAccountAdapter(account)
@@ -19,5 +19,14 @@ describe('CurrentAccountAdapter', () => {
         expect(() => {
             setCurrentAccountAdapter(undefined)
         }).toThrow(new UnexpectedError())
+    })
+
+    test('Should call LocalStorageAdapter.get with correct values', () => {
+        const account = mockAccountModel()
+        const getSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValueOnce(account)
+        const result = getCurrentAccountAdapter()
+
+        expect(getSpy).toHaveBeenCalledWith('account')
+        expect(result).toEqual(account)
     })
 })
