5dfe98a338f64f87e027b30773251e2472737626
Samuel Pereira <meninosam197@gmail.com>
2021-08-15 18:13:29 -0300

    refactor: update faker

--- src/data/test/mock-remote-survey-list.ts
+++ src/data/test/mock-remote-survey-list.ts
@@ -3,10 +3,10 @@ import { RemoteLoadSurveyList } from '@/data/usecases'
 import faker from 'faker'
 
 export const mockRemoteSurveyModel = (): RemoteLoadSurveyList.Model => ({
-    id: faker.random.uuid(),
+    id: faker.datatype.uuid(),
     question: faker.random.words(10),
     date: faker.date.recent().toISOString(),
-    didAnswer: faker.random.boolean()
+    didAnswer: faker.datatype.boolean()
 })
 
 export const mockRemoteSurveyListModel = (): RemoteLoadSurveyList.Model[] => ([
--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -3,10 +3,10 @@ import { LoadSurveyList } from '@/domain/usecases'
 import faker from 'faker'
 
 export const mockSurveyModel = (): LoadSurveyList.Model => ({
-    id: faker.random.uuid(),
+    id: faker.datatype.uuid(),
     question: faker.random.words(10),
     date: faker.date.recent(),
-    didAnswer: faker.random.boolean()
+    didAnswer: faker.datatype.boolean()
 })
 
 export const mockSurveyListModel = (): LoadSurveyList.Model[] => ([
