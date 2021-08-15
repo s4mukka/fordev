2f91d0a56e04e656e91619a72ffbf06e074bee20
Samuel Pereira <meninosam197@gmail.com>
2021-08-15 19:04:23 -0300

    test: ensure SurveyList present error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,21 @@
+import * as Http from '../support/survey-list-mocks'
+import * as Helper from '../support/helpers'
+
+import faker from 'faker'
+
+const populateFields = (): void => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+}
+
+describe('SurveyList', () => {
+    beforeEach(() => {
+        Helper.setLocalStorageItem('account', { accessToken: faker.datatype.uuid(), name: faker.name.findName() })
+    })
+
+    it('Should present error on UnexpectedError', () => {
+        Http.mockUnexpectedError()
+        cy.visit('')
+        cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+    })
+})
