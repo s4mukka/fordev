e923a72bcf411047a5b7cba9c1972c8d8fa44199
Samuel Pereira <meninosam197@gmail.com>
2021-08-17 20:03:23 -0300

    test: ensure SurveyResult go back to SurveyList on back button click

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -16,7 +16,7 @@ type SutTypes = {
 }
 
 const makeSut = (loadSurveyResultSpy = new LoadSurveyResultSpy()): SutTypes => {
-    const history = createMemoryHistory()
+    const history = createMemoryHistory({ initialEntries: ['/', '/surveys/any_id'], initialIndex: 1 })
     const setCurrentAccountMock = jest.fn()
     render(
         <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => mockAccountModel() }}>
@@ -121,5 +121,14 @@ describe('SurveyResult Component', () => {
         await waitFor(() => screen.getByTestId('survey-result'))
 
         expect(loadSurveyResultSpy.callsCount).toBe(1)
+    })
+
+    test('Should go to SurveyList on back button click', async () => {
+        const { history } = makeSut()
+
+        await waitFor(() => screen.getByTestId('survey-result'))
+
+        fireEvent.click(screen.getByTestId('back-button'))
+        expect(history.location.pathname).toBe('/')
     })
 })
