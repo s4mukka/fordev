86a4e868bba8fac469d30a10d7beb2b407824d56
Samuel Pereira <meninosam197@gmail.com>
2021-08-17 19:37:43 -0300

    test: ensure SurveyResult present correct result on success

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -11,13 +11,13 @@ export const mockSurveyResultModel = (): LoadSurveyResult.Model => ({
             answer: faker.random.word(),
             count: faker.datatype.number(),
             percent: faker.datatype.number(),
-            isCurrentAccountAnswer: faker.datatype.boolean()
+            isCurrentAccountAnswer: true
         },
         {
             answer: faker.random.word(),
             count: faker.datatype.number(),
             percent: faker.datatype.number(),
-            isCurrentAccountAnswer: faker.datatype.boolean()
+            isCurrentAccountAnswer: false
         }
     ]
 })
