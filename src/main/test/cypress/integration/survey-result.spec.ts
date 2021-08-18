58ad4bc7df93bc4f07b74aa312310cbc8a47f961
Samuel Pereira <meninosam197@gmail.com>
2021-08-17 21:20:12 -0300

    test: ensure SurveyResult reload on reload button click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -13,4 +13,13 @@ describe('SurveyResult', () => {
         cy.visit('/surveys/any_id')
         cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     })
+
+    it('Should reload on button click', () => {
+        Http.mockUnexpectedError()
+        cy.visit(`/surveys/${faker.datatype.uuid()}`)
+        cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+        Http.mockOk()
+        cy.getByTestId('reload').click()
+        cy.getByTestId('question').should('exist')
+    })
 })
--- src/main/test/cypress/utils/survey-result-mocks.ts
+++ src/main/test/cypress/utils/survey-result-mocks.ts
@@ -1,3 +1,46 @@
 import * as Http from './http-mocks'
 
+import faker from 'faker'
+
 export const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'GET')
+export const mockOk = (): void => Http.mockOk(/surveys/, 'GET', mockSurveyResultModel())
+
+const mockSurveyResultModel = (
+    date: Date = faker.date.past(),
+    question: string = faker.random.words(10),
+    didAnswer: boolean = faker.datatype.boolean()
+): LoadSurveyResult.Model => ({
+    question,
+    date,
+    answers: [
+        {
+            image: faker.image.image(),
+            answer: faker.random.word(),
+            count: faker.datatype.number(),
+            percent: faker.datatype.number(100),
+            isCurrentAccountAnswer: true
+        },
+        {
+            answer: faker.random.word(),
+            count: faker.datatype.number(),
+            percent: faker.datatype.number(100),
+            isCurrentAccountAnswer: false
+        }
+    ]
+})
+
+export namespace LoadSurveyResult {
+    export type Model = {
+        question: string
+        answers: AnswerResult[]
+        date: Date
+    }
+}
+
+type AnswerResult = {
+    image?: string
+    answer: string
+    count: number
+    percent: number
+    isCurrentAccountAnswer: boolean
+}
