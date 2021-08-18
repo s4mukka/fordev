6f836097c0cd376fb33395e1e6b1f58e2619b733
Samuel Pereira <meninosam197@gmail.com>
2021-08-17 21:07:30 -0300

    test: ensure SurveyResult redirects to login if no token is found

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -5,4 +5,9 @@ describe('Login', () => {
         cy.visit('')
         Helper.testUrl('/login')
     })
+
+    it('Should logout if survey-result has no token', () => {
+        cy.visit('/surveys/any_id')
+        Helper.testUrl('/login')
+    })
 })
