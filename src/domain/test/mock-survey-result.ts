31899ce57f6d4c58142439f410f9a4bccf7ba44e
Samuel Pereira <meninosam197@gmail.com>
2021-08-18 15:03:47 -0300

    test: ensure SurveyResult prevent multiple answer click

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -39,10 +39,12 @@ export class LoadSurveyResultSpy implements LoadSurveyResult {
 
 export class SaveSurveyResultSpy implements SaveSurveyResult {
     params: SaveSurveyResult.Params
+    callsCount = 0
     surveyResult = mockSurveyResultModel()
 
     async save (params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
         this.params = params
+        this.callsCount++
         return this.surveyResult
     }
 }
